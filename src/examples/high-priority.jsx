fdd6cfbe65f790a31d8a975a8f0af8c58d10e9a0
baymax <476803711@qq.com>
2023-04-03 17:41:01 +0800

    high priority

--- src/examples/high-priority.jsx
+++ src/examples/high-priority.jsx
@@ -1,22 +1,27 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export function Counter() {
   const [count, setCount] = useState(0);
+  const buttonRef = useRef();
+
+  const onClick = () => {
+    setCount((c) => c + 2);
+  };
 
   useEffect(() => {
     setTimeout(() => {
-      setCount(2);
+      setCount(1);
     }, 1000);
 
     setTimeout(() => {
-      setCount(3);
+      buttonRef.current.click();
     }, 1020);
   }, []);
 
   console.log(count);
 
   return (
-    <ul onClick={() => setCount((c) => c + 1)}>
+    <ul ref={buttonRef} onClick={onClick}>
       {new Array(10000).fill(0).map((_, i) => (
         <span key={i}>{count}</span>
       ))}
