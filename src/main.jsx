8f3382a9a63eed03e1d7265708ceb0d7e4dd97c7
baymax <476803711@qq.com>
2023-04-05 00:10:04 +0800

    counter

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { useState, Component } from "react";
-import { Counter } from "./examples/high-priority";
+import { Counter } from "./examples/counter";
 
 createRoot(document.getElementById("root")).render(<Counter />);
